9a940815b35a49d9278c911459ccc3ce88490655
tangjiapeng <tangjiapeng@itjuzi.com>
2025-03-17 18:27:52 +0800

    Initial Commit

---
+++ iOS-Network-Stack-Dive/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iOS-Network-Stack-Dive
+//
+//  Created by 唐佳鹏 on 2025/3/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS-Network-Stack-Dive/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  iOS-Network-Stack-Dive
+//
+//  Created by 唐佳鹏 on 2025/3/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
