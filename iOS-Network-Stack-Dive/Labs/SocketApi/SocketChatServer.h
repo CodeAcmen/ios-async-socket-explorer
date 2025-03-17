89fd6ebeebb789d350c5ec9d8b6f88ab5e8570a2
tangjiapeng <tangjiapeng@itjuzi.com>
2025-03-17 22:48:15 +0800

    initialize project

---
+++ iOS-Network-Stack-Dive/Labs/SocketApi/SocketChatServer.h
@@ -0,0 +1,16 @@
+//
+//  SocketChatServer.h
+//  iOS-Network-Stack-Dive
+//
+//  Created by 唐佳鹏 on 2025/3/17.
+//  基于TCP的文本聊天实现
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SocketChatServer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS-Network-Stack-Dive/Labs/SocketApi/SocketChatServer.m
@@ -0,0 +1,12 @@
+//
+//  SocketChatServer.m
+//  iOS-Network-Stack-Dive
+//
+//  Created by 唐佳鹏 on 2025/3/17.
+//
+
+#import "SocketChatServer.h"
+
+@implementation SocketChatServer
+
+@end
